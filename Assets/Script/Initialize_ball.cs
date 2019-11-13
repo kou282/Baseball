ae756bc9c94bc4b7dc864478af0e37bb9a227aaf
大村洸平 <kohei282@gmail.com>
2019-11-14 04:17:12 +0900

    守備の配置

---
+++ Assets/Script/Initialize_ball.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Initialize_ball : MonoBehaviour
+{
+  private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter(Collision collision){
+      if(collision.gameObject.tag!="Bat" & collision.gameObject.tag!="Ground"){//BatとGround以外に触れた時
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        transform.position = new Vector3(0.3f, 0.5f, -25f);
+        Pitching.next=1;
+      }
+    }
+}
---
+++ Assets/Script/Pitching.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pitching : MonoBehaviour
+{
+  private float timer = 0.0f;
+  private float nextball = 1.0f;
+  public static int  next = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+      next = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(next==1){
+        timer += Time.deltaTime;
+      }
+      if((timer >nextball) & next==1) {
+        float s = 4000;//speed
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.AddForce(0, 0, -s);//打ち出す
+
+        timer = 0.0f;
+        next = 0;
+      }
+    }
+}
